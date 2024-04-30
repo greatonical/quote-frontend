6d34e7ac682fc926f2e9849020ae76c2027bd249
calledAdo <destinyadokiye8@gmail.com>
2024-04-30 11:48:26 +0100

    Pull commit

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from "react";
 import { Button } from "@components";
-import {
-  
-  walletConnection,
-  checkConnectedWallet,
-} from "../../lib/utils";
+import { walletConnection, checkConnectedWallet } from "../../lib/utils";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
-const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [ethInput, setEthInput] = useState("");
   const [quoteInput, setQuoteInput] = useState("");
@@ -17,7 +13,6 @@ const navigate = useNavigate()
   const [connected, setConnected] = useState(false);
   const [currentAccount, setCurrentAccount] = useState();
   //   const [account] = useState();
-
 
   const [ethValue, setEthValue] = useState("");
   //@ts-ignore
@@ -40,7 +35,6 @@ const navigate = useNavigate()
     walletConnection(setConnected);
     checkConnectedWallet(setCurrentAccount, setConnected);
   }, [currentAccount, connected]);
-
 
   useEffect(() => {
     const convertValue = async () => {
@@ -111,10 +105,15 @@ const navigate = useNavigate()
           <div className={`bg-white opacity-15 w-full h-[0.5px] mt-4`} />
         </div>
 
-<div className="w-full flex flex-col items-center mt-10">
-<Button text="Create CDP" className="w-[50%]" onClick={()=>{navigate("/providers")}}/>
-</div>
-     
+        <div className="w-full flex flex-col items-center mt-10">
+          <Button
+            text="Create CDP"
+            className="w-[50%]"
+            onClick={() => {
+              navigate("/providers");
+            }}
+          />
+        </div>
       </body>
     </main>
   );
--- src/pages/app/providers.tsx
+++ src/pages/app/providers.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { AppHeader, Line } from "@components";
 import { openLink } from "../../lib/utils";
 import { Icon } from "@iconify/react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import { METAMASK_LINK, TRUST_WALLET_LINK } from "@constants";
 import Pool from "./pool";
 
@@ -78,61 +78,81 @@ export default function Provider() {
   const [route, setRoute] = useState<string>("Dashboard");
   const [showWallet, setShowWallet] = useState<boolean>(false);
 
-  const Providers = () =>{
-    return(  <main className="flex flex-col pt-40 px-24">
-    <body className="">
-    {showWallet && (
-    <motion.section className="h-[95%] mt-4 mb-20 w-72 absolute right-5 z-50 p-4 shadow-lg rounded-2xl bg-white border border-neutral-200" initial={{ x: 100 }}
-    whileInView={{ x: 0}}
-    viewport={{ once: true }}
-    transition={{ mass: 200, duration: 0.2, ease: "easeInOut", delay: 0 }}>
-      <span className="flex flex-row w-full justify-between">
-        <p className="font-satoshi-medium text-lg">Connect Wallet</p>
-        <Icon icon={"formkit:close"} onClick={()=>{setShowWallet(false)}} className="w-6 h-6 text-black" />
-      </span>
-      <button
-        className="flex flex-row gap-x-3 mt-7 items-center"
-        onClick={() => {
-          openLink(METAMASK_LINK);
-        }}
-      >
-        <img src="./metamask.svg" className="w-8 h-8" />
-        <p className="font-satoshi-medium text-lg">Metamask</p>
-      </button>
-      <Line className="w-full mt-2" />
+  const Providers = () => {
+    return (
+      <main className="flex flex-col pt-40 px-24">
+        <body className="">
+          {showWallet && (
+            <motion.section
+              className="h-[95%] mt-4 mb-20 w-72 absolute right-5 z-50 p-4 shadow-lg rounded-2xl bg-white border border-neutral-200"
+              initial={{ x: 100 }}
+              whileInView={{ x: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                mass: 200,
+                duration: 0.2,
+                ease: "easeInOut",
+                delay: 0,
+              }}
+            >
+              <span className="flex flex-row w-full justify-between">
+                <p className="font-satoshi-medium text-lg">Connect Wallet</p>
+                <Icon
+                  icon={"formkit:close"}
+                  onClick={() => {
+                    setShowWallet(false);
+                  }}
+                  className="w-6 h-6 text-black"
+                />
+              </span>
+              <button
+                className="flex flex-row gap-x-3 mt-7 items-center"
+                onClick={() => {
+                  openLink(METAMASK_LINK);
+                }}
+              >
+                <img src="./metamask.svg" className="w-8 h-8" />
+                <p className="font-satoshi-medium text-lg">Metamask</p>
+              </button>
+              <Line className="w-full mt-2" />
 
-      <button
-        className="flex flex-row gap-x-3 mt-10 items-center"
-        onClick={() => {
-          openLink(TRUST_WALLET_LINK);
-        }}
-      >
-        <img src="./trust_wallet.svg" className="w-8 h-8" />
-        <p className="font-satoshi-medium text-lg">Trust Wallet</p>
-      </button>
-      <Line className="w-full mt-2" />
-      <a href="https://github.com/greatonical/quote-stablecoin/blob/frontend/README.md#how-to-guides" target="_blank" className="text-primary absolute bottom-5 underline flex flex-row">
-        View Minting guide here
-        <Icon icon={"octicon:arrow-up-right-24"} className="w-5 h-5"/>
-      </a>
-    </motion.section>
-  )}
+              <button
+                className="flex flex-row gap-x-3 mt-10 items-center"
+                onClick={() => {
+                  openLink(TRUST_WALLET_LINK);
+                }}
+              >
+                <img src="./trust_wallet.svg" className="w-8 h-8" />
+                <p className="font-satoshi-medium text-lg">Trust Wallet</p>
+              </button>
+              <Line className="w-full mt-2" />
+              <a
+                href="https://github.com/greatonical/quote-stablecoin/blob/frontend/README.md#how-to-guides"
+                target="_blank"
+                className="text-primary absolute bottom-5 underline flex flex-row"
+              >
+                View Minting guide here
+                <Icon icon={"octicon:arrow-up-right-24"} className="w-5 h-5" />
+              </a>
+            </motion.section>
+          )}
 
-      <object className="flex flex-row justify-between">
-        <p className="dark:text-white text-5xl">Providers</p>
-      </object>
-      <div className="dark:bg-holder p-5 rounded-xl mt-7">
-        <header className="w-full grid grid-cols-4 text-lg text-neutral-400 mt-1">
-          <p className="">Name of provider</p>
-          <p className=" w-96">Min Duration</p>
-          <p className=" w-96">Interest Rate</p>
-          <p className=" w-96">Max Drawdown</p>
-        </header>
-        <div className={`bg-white opacity-15 w-full h-[0.5px] mt-4`} />
-      </div>
-    </body>
-  </main>)
-  }
+          <object className="flex flex-row justify-between">
+            <p className="dark:text-white text-5xl">Providers</p>
+          </object>
+          <div className="dark:bg-holder p-5 rounded-xl mt-7">
+            <header className="w-full grid grid-cols-4 text-lg text-neutral-400 mt-1">
+              <p className="">Name of provider</p>
+              <p className=" w-96">Min Duration</p>
+              <p className=" w-96">Interest Rate</p>
+              <p className=" w-96">Max Drawdown</p>
+            </header>
+            <div className={`bg-white opacity-15 w-full h-[0.5px] mt-4`} />
+          </div>
+        </body>
+      </main>
+    );
+  };
 
   return (
     <div className="flex flex-col h-full relative">
@@ -142,8 +162,7 @@ export default function Provider() {
         route={route}
         setRoute={setRoute}
       />
-      {route === "Dashboard" ? (<Providers/>):(<Pool/>)}
-    
+      {route === "Dashboard" ? <Providers /> : <Pool />}
     </div>
   );
 }
